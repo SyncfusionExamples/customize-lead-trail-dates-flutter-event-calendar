9b4ca88e4dd6bfbb44b0ecc9aab7fa6e51767528
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-10-26 17:45:51 +0530

    FLUT-2631 - Sample added.

---
+++ android/app/src/main/kotlin/com/example/flutter_leadtraildates_custom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_leadtraildates_custom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(LeadingTrailingDatesCustomization());
+
+class LeadingTrailingDatesCustomization extends StatefulWidget {
+  @override
+  LeadingTrailingDatesCustomizationState createState() => LeadingTrailingDatesCustomizationState();
+}
+
+class LeadingTrailingDatesCustomizationState extends State<LeadingTrailingDatesCustomization> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: SafeArea(
+          child: SfCalendar(
+            view: CalendarView.month,
+            dataSource: _getDataSource(),
+            monthCellBuilder: monthCellBuilder,
+          ),
+        )));
+  }
+
+  _DataSource _getDataSource() {
+    final List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(hours: 4)),
+      endTime: DateTime.now().add(Duration(hours: 5)),
+      subject: 'Meeting',
+      color: Colors.red,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 5)),
+      subject: 'Development Meeting   New York, U.S.A',
+      color: Color(0xFFf527318),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      subject: 'Project Plan Meeting   Kuala Lumpur, Malaysia',
+      color: Color(0xFFfb21f66),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      subject: 'Support - Web Meeting   Dubai, UAE',
+      color: Color(0xFFf3282b8),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 1)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      subject: 'Project Release Meeting   Istanbul, Turkey',
+      color: Color(0xFFf2a7886),
+    ));
+    appointments.add(Appointment(
+        startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
+        endTime: DateTime.now().add(const Duration(hours: 5, days: -1)),
+        subject: 'Release Meeting',
+        color: Colors.lightBlueAccent,
+        isAllDay: true));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 2, days: -4)),
+      endTime: DateTime.now().add(const Duration(hours: 4, days: -4)),
+      subject: 'Performance check',
+      color: Colors.amber,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 11, days: -2)),
+      endTime: DateTime.now().add(const Duration(hours: 12, days: -2)),
+      subject: 'Customer Meeting   Tokyo, Japan',
+      color: Color(0xFFffb8d62),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
+      endTime: DateTime.now().add(const Duration(hours: 7, days: 2)),
+      subject: 'Retrospective',
+      color: Colors.purple,
+    ));
+
+    return _DataSource(appointments);
+  }
+
+  Widget monthCellBuilder(BuildContext context, MonthCellDetails details) {
+    var mid=details.visibleDates.length~/2.toInt();
+    var midDate=details.visibleDates[0].add(Duration(days: mid));
+
+    if(details.date.month != midDate.month){
+      return Container(
+        color: Colors.pinkAccent,
+        child: Text(
+          details.date.day.toString(),
+        ),
+      );
+    }
+    else
+      {
+        return Container(
+          color: Colors.green,
+          child: Text(
+            details.date.day.toString(),
+          ),
+        );
+      }
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
